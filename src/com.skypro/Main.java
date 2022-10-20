b1510faac780b6884b30cb578d618c4c83baa230
JohanNayvert <111690307+JohanNayvert@users.noreply.github.com>
2022-10-20 06:19:25 +0500

    First Commit

---
+++ src/com.skypro/Main.java
@@ -0,0 +1,12 @@
+package com.skypro;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Methods");
+        System.out.println();
+
+
+
+        System.out.println("HomeWork Complete");
+    }
+}
